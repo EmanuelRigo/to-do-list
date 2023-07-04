b734e9ba17cb8f9fea8594d48b5073d6ff31bc8a
EmanuelRigo <112441019+EmanuelRigo@users.noreply.github.com>
2023-07-04 11:47:52 -0300

    primer commit

---
+++ src/data/task.js
@@ -0,0 +1,14 @@
+export const tasks = [
+   {
+      id: 0,
+      title: "mi primer tarea",
+      description: "labar"
+   }, {
+      id: 1,
+      title: "mi segunda tarea",
+      description: "escribir"
+   }, {
+      id: 2,
+      title: "mi tercer tarea",
+      description: "leer"
+   },]
